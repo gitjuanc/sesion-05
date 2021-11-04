79ea4a7dbdaca63a8254f0fc923f347cc1d00f81
Juan Carlos Suarez Cifuentes <juan.suarez@novatec.loc>
2021-11-04 10:35:21 -0500

    expreciones regulares actividad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sesion03';
+ 
+  title="actividad";
+  vericorreo:string="";
+  verificacion:string="";
+
+  veritexto:string="";
+  validaciontex:string="";
+
+
+
+  verificacioncorreo(correo:string){
+    this.vericorreo=correo
+
+    if(/\w+\@\w+\.\w+/.test(correo)){
+      this.verificacion="correo valido"
+    }else{
+      this.verificacion="correo invalido"
+    }
+
+    console.log("El correo es: ",this.vericorreo);
+  }
+
+  validacion(textov:string){
+    this.veritexto=textov
+
+    if(/\@\d{5}\@\d{6}\@\w{7}\@/.test(textov)){
+      this.validaciontex="texto valido"
+    }else{
+      this.validaciontex="texto invalido"
+    }
+
+    console.log("El correo es: ",this.veritexto);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,11 +8,13 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
